1f0a55b9a1604f1ddf72dc0a74afc01853517e9f
Ciro Nunes <ciroanunes@gmail.com>
2014-06-26 21:58:37 -0300

    feat(share): add share on facebook

--- app/share/shareService.js
+++ app/share/shareService.js
@@ -2,22 +2,39 @@
 
 angular.module('bt.share')
   .service('share', ['$window', function($window) {
+    var MESSAGE = 'Here goes my #WorldCupBets: ';
+
     function formatBetMessage(match) {
       return match.home_team.code + ' ' + match.home_team.bet + ' x ' + match.away_team.bet + ' ' + match.away_team.code;
     }
 
     function openTwitterShare(msg) {
-      $window.open('https://twitter.com/share?text=' + encodeURIComponent(msg), 'twitter-share', 'height=440,width=720');
+      $window.open(
+        'https://twitter.com/share?text=' + encodeURIComponent(msg),
+        'twitter-share',
+        'height=440, width=720'
+      );
     }
 
-    this.shareOnTwitter = function(bets) {
-      var msgs = [],
-        i;
+    function openFacebookShare(msg) {
+      $window.open(
+        'http://www.facebook.com/sharer.php?s=100&' +
+        'p[title]=title&p[url]=http://www.worldcupbets.com&' +
+        'p[summary]=' + encodeURIComponent(msg) +
+        'summary&p[images][0]= ' +
+        'http://upload.wikimedia.org/wikipedia/en/thumb/e/e8/WC-2014-Brasil.svg/719px-WC-2014-Brasil.svg.png',
+        'facebook-share',
+        'height=440, width=720'
+      );
+    };
 
-      for (i = 0; i < bets.length; i++) {
-        msgs.push(formatBetMessage(bets[i]));
-      }
+    this.shareOnTwitter = function(bet) {
+      bet = formatBetMessage(bet);
+      openTwitterShare(MESSAGE + bet);
+    };
 
-      openTwitterShare('Here goes my #WorldCupBets: ' + msgs.join(' - ') + '.');
+    this.shareOnFacebook = function(bet) {
+      bet = formatBetMessage(bet);
+      openFacebookShare(MESSAGE + bet);
     };
   }]);
--- test/unit/share/shareServiceSpec.js
+++ test/unit/share/shareServiceSpec.js
@@ -27,29 +27,25 @@ describe('share service', function() {
     };
   }
 
-  it('should create a twitter message for each bet, and join them by " - "', function() {
-    share.shareOnTwitter([
-      bet('BRA', 1, 'CHI', 2)
-    ]);
+  describe('#twitter', function() {
+    it('should create a twitter message for each bet, and join them by " - "', function() {
+      share.shareOnTwitter(bet('BRA', 1, 'CHI', 2));
 
-    expect(getLastMessage().indexOf('BRA 1 x 2 CHI')).not.toBe(-1);
+      expect(getLastMessage().indexOf('BRA 1 x 2 CHI')).not.toBe(-1);
+    });
 
-    share.shareOnTwitter([
-      bet('BRA', 1, 'CHI', 2),
-      bet('DEF', 3, 'ABC', 4)
-    ]);
+    it('should keep the message under 140', function() {
+      share.shareOnTwitter(bet('BRA', 1, 'CHI', 2));
 
-    expect(getLastMessage().indexOf('BRA 1 x 2 CHI - DEF 3 x 4 ABC')).not.toBe(-1);
+      expect(getLastMessage().length).toBeLessThan(140);
+    });
   });
 
-  it('should keep the message under 140 chars with 4 matches', function() {
-    share.shareOnTwitter([
-      bet('BRA', 1, 'CHI', 2),
-      bet('DEF', 3, 'ABC', 4),
-      bet('GHI', 5, 'JKL', 6),
-      bet('MNF', 7, 'OPQ', 8)
-    ]);
+  describe('#facebook', function() {
+    it('should create a message for a bet', function() {
+      share.shareOnFacebook(bet('BRA', 1, 'CHI', 2));
 
-    expect(getLastMessage().length).toBeLessThan(140);
+      expect(getLastMessage().indexOf('BRA 1 x 2 CHI')).not.toBe(-1);
+    });
   });
 });
