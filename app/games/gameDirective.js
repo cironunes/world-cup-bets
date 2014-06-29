21e3f056e060ee1e3563f507d8528e659da7cff6
Caio Cunha <caio@caiotoon.com>
2014-06-29 12:34:48 -0400

    feat(game): implemented skip match feature

--- app/games/gameDirective.js
+++ app/games/gameDirective.js
@@ -1,10 +1,25 @@
 'use strict';
 
-angular.module('bets')
+angular.module('bt.games')
   .directive('btGame', function() {
     return {
       restrict: 'E',
       scope: { match: '=' },
+      controller: function($scope) {
+        $scope.toggleSkipMatch = function(match) {
+          match.skipped = !match.skipped;
+
+          if (match.skipped) {
+            match.home_team.bkpBet = match.home_team.bet;
+            match.away_team.bkpBet = match.away_team.bet;
+            match.home_team.bet = undefined;
+            match.away_team.bet = undefined;
+          } else {
+            match.home_team.bet = match.home_team.bkpBet;
+            match.away_team.bet = match.away_team.bkpBet;
+          }
+        };
+      },
       templateUrl: 'games/game.tpl.html'
     };
   });
