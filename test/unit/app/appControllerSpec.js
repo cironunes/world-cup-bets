1b765b540bb91ef3abeca1547fbb5e0fe130eb16
Ciro Nunes <ciroanunes@gmail.com>
2014-07-06 11:06:01 -0300

    test(appController): exercise undefined value

--- test/unit/app/appControllerSpec.js
+++ test/unit/app/appControllerSpec.js
@@ -15,31 +15,43 @@ describe('Ctrl: AppCtrl', function() {
     $controller('AppCtrl', {$scope: scope});
   }));
 
-  it('should return true if length of valid bets is greater than 0', function() {
-    var games = {};
+  describe('#hasValidBets', function() {
+    it('should return true if length of valid bets is greater than 0', function() {
+      var games = {};
 
-    validBet.andReturn([1]);
-    scope.hasValidBets(games);
+      validBet.andReturn([1]);
+      scope.hasValidBets(games);
 
-    expect(scope.hasValidBets(games)).toBe(true);
-    expect(validBet).toHaveBeenCalledWith(games);
-  });
+      expect(scope.hasValidBets(games)).toBe(true);
+      expect(validBet).toHaveBeenCalledWith(games);
+    });
 
-  it('should return false if length of valid bets is equal to 0', function() {
-    var games = {};
+    it('should return false if length of valid bets is equal to 0', function() {
+      var games = {};
 
-    validBet.andReturn([]);
-    scope.hasValidBets(games);
+      validBet.andReturn([]);
+      scope.hasValidBets(games);
 
-    expect(scope.hasValidBets(games)).toBe(false);
-  });
+      expect(scope.hasValidBets(games)).toBe(false);
+    });
 
-  it('should return false if return is null or undefined', function() {
-    var games = {};
+    it('should return false if return is null', function() {
+      var games = {};
 
-    validBet.andReturn(null);
-    scope.hasValidBets(games);
+      validBet.andReturn(null);
+      scope.hasValidBets(games);
 
-    expect(scope.hasValidBets(games)).toBe(false);
+      expect(scope.hasValidBets(games)).toBe(false);
+    });
+
+    it('should return false if return is undefined', function() {
+      var games = {};
+
+      validBet.andReturn(undefined);
+      scope.hasValidBets(games);
+
+      expect(scope.hasValidBets(games)).toBe(false);
+    });
   });
 });
+
